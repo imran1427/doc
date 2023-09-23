fcb0b48534acb5c7045b33fb3e38ee303b666f9e
rashed9919 <rashedcse233@gmail.com>
2023-09-23 15:59:16 +0600

    doctt

---
+++ Doc/Model/Doctor.cs
@@ -0,0 +1,9 @@
+﻿namespace Doc.Model
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
